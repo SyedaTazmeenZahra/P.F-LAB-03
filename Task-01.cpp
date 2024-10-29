020ad05170c0905f86123a5ad936e8f1f999041f
SyedaTazmeenZahra <sofiasareem07@gmail.com>
2024-10-29 20:45:21 +0500

    These are tasks from Lab 03

---
+++ Task-01.cpp
@@ -0,0 +1,15 @@
+//Working with different data types
+#include <iostream>
+using namespace std;
+int main() {
+    int digit = 10;
+    float decimal = 5.7;
+    char letter = 'T';
+    bool t_f = true;
+
+    cout << "Integer is: " << digit << '\n';
+    cout << "Decimal is: " << decimal << '\n';
+    cout << "Character is: " << letter << '\n';
+    cout << "Boolean is: " << t_f << '\n';
+    return 0;
+}
